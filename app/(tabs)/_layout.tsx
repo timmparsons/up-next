71b245dcc8e1351d15c178e29354bf3545def28c
timmparsons <tim.m.parsons@hotmail.com>
2024-10-09 07:29:24 -0700

    added new tab styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,49 @@
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'black',
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 30,
+          height: 60,
+          width: '80%',
+          left: '10%',
+          alignSelf: 'center',
+          borderRadius: 30,
+          backgroundColor: '#ffffff',
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+          elevation: 5,
+          paddingBottom: 0,
+        },
+        tabBarActiveTintColor: '#007aff',
+        tabBarInactiveTintColor: 'gray',
+        tabBarIconStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 9,
+          fontWeight: 'bold',
+          paddingTop: 0,
+          marginTop: -7,
+          marginBottom: 5,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="home" size={20} color="lightgray" />,
           // headerRight: () => (
           //   <Link href="/modal" asChild>
           //     <HeaderButton />
@@ -25,21 +55,23 @@ export default function TabLayout() {
         name="list"
         options={{
           title: 'List',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="list" size={20} color="lightgray" />,
         }}
       />
       <Tabs.Screen
-        name="my-movies"
+        name="myMovies"
         options={{
           title: 'My Movies',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="file-movie-o" size={20} color="lightgray" />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="meh" size={20} color="lightgray" />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,9 @@
+import { Stack } from 'expo-router';
+
+export default function List() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'List' }} />
+    </>
+  );
+}
