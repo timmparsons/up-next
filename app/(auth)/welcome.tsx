c9fcb764d0da3e35ae7a43bca7d02a48acb008c6
timmparsons <tim.m.parsons@hotmail.com>
2024-10-24 07:14:12 -0700

    added sign in screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,18 @@
 import { Stack, useRouter } from 'expo-router';
 import React from 'react';
-import { Text, SafeAreaView, Pressable } from 'react-native';
+import { Text, SafeAreaView, Pressable, View } from 'react-native';
 
 import Button from '~/components/Button';
 
 const Auth = () => {
   const router = useRouter();
 
-  const buttonPress = () => {
+  const redirectToLogin = () => {
     router.push('/(auth)/login');
+  };
+
+  const redirectToSignIn = () => {
+    router.push('/(auth)/signIn');
   };
 
   return (
@@ -19,9 +23,18 @@ const Auth = () => {
         <Text className="mb-8 text-lg">Find out what your friends are watching</Text>
         <Pressable
           className="rounded-xl border border-orange-500 bg-orange-500 p-5"
-          onPress={buttonPress}>
+          onPress={redirectToSignIn}>
           <Text className="text-lg font-bold text-white">Get Started</Text>
         </Pressable>
+
+        <View className="mt-4" style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text>Already have an account?</Text>
+          <Pressable onPress={redirectToLogin}>
+            <Text className="px-2" style={{ textDecorationLine: 'underline' }}>
+              Log In
+            </Text>
+          </Pressable>
+        </View>
       </SafeAreaView>
     </>
   );
